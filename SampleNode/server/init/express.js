91bf1d8c1181f974bdbc27649cb1a8afa3d7e494
Charles Castillo <charlescastillo1982@yahoo.com>
2015-08-17 19:10:53 -0400

    Sample Node App

---
+++ SampleNode/server/init/express.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var bodyParser = require('body-parser');
+
+module.exports = function(db) {
+  var app = express();
+  app.use(bodyParser.json());
+
+  // Load in Database models
+  require('../models/todo.model');
+
+  app.use(express.static('./dist'));
+
+  // Load in Route handlers
+  require('../routes/todo.routes')(app);
+  require('../routes/gif.routes')(app);
+
+  return app;
+}
